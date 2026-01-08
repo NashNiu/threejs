ef86384ce99ab82a83db587808483597745d3322
nash <545100402@qq.com>
2026-01-08 22:31:27 +0800

    feat: add GSAP for animations and implement cube movement in Three.js scene

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import gsap from 'gsap';
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -11,8 +11,30 @@ const size = {
     height: 600
 };
 const camera = new THREE.PerspectiveCamera(45, size.width / size.height, 0.1, 1000);
-camera.position.z = 3;
+camera.position.z = 5;
 const canvasDom = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas: canvasDom });
 renderer.setSize(size.width, size.height);
-renderer.render(scene, camera);
+
+// const time = Date.now();
+// const tick = () => {
+//     const currentTime = Date.now();
+//     const deltaTime = currentTime - time;   
+//     cube.rotation.y = 0.001 * deltaTime;
+//     renderer.render(scene, camera);
+//     requestAnimationFrame(tick);
+// }
+// tick();
+gsap.to(cube.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(cube.position, { duration: 1, delay: 2, x: 0 });
+const clock = new THREE.Clock();
+const tick = () => {
+    // const elapsedTime = clock.getElapsedTime();
+    // // cube.rotation.y = elapsedTime;
+    // cube.position.y = Math.sin(elapsedTime);
+    // // cube.rotation.y = elapsedTime;
+    // cube.position.x = Math.cos(elapsedTime);
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+};
+tick();
