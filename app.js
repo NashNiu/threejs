baa18509fe4767af779beeb86e0d2f44717e6a2c
niutengfei <niutengfei@magiccompass.cn>
2026-01-08 15:14:35 +0800

    refactor: update cube position and set fixed canvas size for rendering

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-
+cube.position.x = 1;
+cube.position.y = -2;
+cube.position.z = 0.5;
 scene.add(cube);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const size = {
+    width: 800,
+    height: 600
+};
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
 camera.position.z = 5;
 const canvasDom = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas: canvasDom });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(size.width, size.height);
 renderer.render(scene, camera);
