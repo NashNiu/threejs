d82699f38a2a3edd6834a2f62f25a654d0cb7798
nash <545100402@qq.com>
2026-01-22 23:24:16 +0800

    feat(render): add sky and fog

--- src/house.js
+++ src/house.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Timer } from "three/addons/misc/Timer.js";
+import { Sky } from 'three/addons/objects/Sky.js'
 import Router from "./router.js";
 import GUI from "lil-gui";
 
@@ -364,6 +365,21 @@ function initThreeScene() {
   ghost3.shadow.mapSize.width = 256
   ghost3.shadow.mapSize.height = 256
   ghost3.shadow.camera.far = 10
+
+  /**
+   * Sky
+   */
+  const sky = new Sky()
+  sky.scale.set(100, 100, 100)
+  scene.add(sky)
+  sky.material.uniforms['turbidity'].value = 10
+  sky.material.uniforms['rayleigh'].value = 3
+  sky.material.uniforms['mieCoefficient'].value = 0.1
+  sky.material.uniforms['mieDirectionalG'].value = 0.95
+  sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95)
+
+
+  scene.fog = new THREE.FogExp2('#04343f', 0.1)
 
   window.addEventListener("resize", () => {
     sizes.width = window.innerWidth;
