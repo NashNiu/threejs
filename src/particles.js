9d7ac66dcb88b0261cc7c7f21997e4beba4ae1d7
niutengfei <niutengfei@magiccompass.cn>
2026-01-23 16:31:16 +0800

    feat(particles): enhance particle system with buffer geometry and custom textures

--- src/particles.js
+++ src/particles.js
@@ -24,19 +24,40 @@ function initParticlesScene() {
     scene = new THREE.Scene();
 
     const textureLoader = new THREE.TextureLoader();
+    const particleTexture = textureLoader.load('./particles/2.png');
 
     /**
      * particles
      */
-    const particleGeometry = new THREE.SphereGeometry(1, 32, 32);
+    const particleGeometry = new THREE.BufferGeometry();
+    const count = 5000;
+    // 3: x y z
+    const positions = new Float32Array(count * 3);
+    // 3: r g b
+    const colors = new Float32Array(count * 3);
+    for (let i = 0; i < count * 3; i++) {
+        positions[i] = (Math.random() - 0.5) * 10;
+        colors[i] = Math.random();
+    }
+    particleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    particleGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
     const particleMaterial = new THREE.PointsMaterial({
-        size: 0.02,
-        sizeAttenuation: true
+        // color: 0xff88cc,
+        size: 0.1,
+        sizeAttenuation: true,
+        alphaMap: particleTexture,
+        transparent: true,
+        // alphaTest: 0.001,
+        // depthTest: false,
+        // good solution
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true,
     })
     // points
     const particles = new THREE.Points(particleGeometry, particleMaterial)
     scene.add(particles)
-    
     /**
      * Sizes
      */
