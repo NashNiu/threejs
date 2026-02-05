3217501575597b4638e126c8bfa5f26dd3d5679a
niutengfei <niutengfei@magiccompass.cn>
2026-02-05 18:03:17 +0800

    refactor(fox): extract camera, renderer, and world into modular classes

--- src/fox/fox.js
+++ src/fox/fox.js
@@ -1,11 +1,25 @@
+import * as THREE from "three";
 import Sizes from "./sizes";
 import Time from "./time";
+import Camera from "./camera";
+import Renderer from "./renderer";
+import World from "./world";
+let instance;
 export default class Fox {
   constructor(canvas) {
+    // singleton
+    if (instance) {
+      return instance;
+    }
+    instance = this;
     console.log("canvas", canvas);
     this.canvas = canvas;
     this.sizes = new Sizes();
     this.time = new Time();
+    this.scene = new THREE.Scene();
+    this.camera = new Camera();
+    this.renderer = new Renderer();
+    this.world = new World();
     this.sizes.on("resize", () => {
       this.resize();
     });
@@ -15,10 +29,12 @@ export default class Fox {
     });
   }
   resize() {
-    console.log("resized");
+    this.camera.resize();
+    this.renderer.resize();
   }
   update() {
-    // console.log("ticked");
+    this.camera.update();
+    this.renderer.update();
   }
 }
 
