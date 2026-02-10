cde76f44cd6c7150b75244c4406651db85b0e394
nash <545100402@qq.com>
2026-02-10 23:31:43 +0800

    refactor: reorganize project structure and add resource management

---
+++ src/fox/utils/resources.js
@@ -0,0 +1,65 @@
+import EventEmitter from "./eventEmitter";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "three";
+export default class Resources extends EventEmitter {
+    constructor(sources) {
+        super();
+        this.sources = sources;
+        // setup
+        this.items = {};
+        this.toLoad = this.sources.length;
+        this.loaded = 0;
+        this.setLoaders()
+        this.startLoading();
+    }
+    setLoaders() {
+        this.loaders = {};
+        this.loaders["gltfLoader"] = new GLTFLoader();
+        this.loaders["textureLoader"] = new THREE.TextureLoader();
+        this.loaders["cubeTextureLoader"] = new THREE.CubeTextureLoader();
+    }
+    startLoading() {
+        // load each source
+        for (const source of this.sources) {
+            if (source.type === "gltfModel") {
+                this.loaders['gltfLoader'].load(source.path, (file) => {
+                    this.sourceLoaded(source, file)
+                    // this.items[source.name] = file;
+                    // this.loaded++;
+                    // if(this.loaded === this.toLoad){
+                    //     this.trigger("ready");
+                    // }
+                });
+            } else if (source.type === "texture") {
+                this.loaders['textureLoader'].load(source.path, (file) => {
+                    this.sourceLoaded(source, file)
+                    console.log(source.name, file);
+                    // this.items[source.name] = file;
+                    // this.loaded++;
+                    // if(this.loaded === this.toLoad){
+                    //     this.trigger("ready");
+                    // }
+                });
+            } else if (source.type === "cubeTexture") {
+                this.loaders['cubeTextureLoader'].load(source.path, (file) => {
+                    this.sourceLoaded(source, file)
+                    // this.items[source.name] = file;
+                    // this.loaded++;
+                    // if(this.loaded === this.toLoad){
+                    //     this.trigger("ready");
+                    // }
+                });
+            }
+        }
+    }
+
+    sourceLoaded(source, file) {
+        this.items[source.name] = file;
+        this.loaded++;
+        if(this.loaded === this.toLoad){
+            console.log("all loaded");
+            this.trigger("ready");
+        }
+    }
+
+}
---
+++ src/fox/world/environment.js
@@ -0,0 +1,41 @@
+import * as THREE from "three";
+import Fox from "../fox";
+
+
+export default class Environment {
+    constructor() {
+        this.fox = new Fox()
+        this.scene = this.fox.scene
+        this.resources = this.fox.resources
+        this.setSunLight()
+        this.setEnvironmentMap()
+    }
+    setSunLight() {
+        this.sunLight = new THREE.DirectionalLight("#ffffff", 4);
+        this.sunLight.castShadow = true;
+        this.sunLight.shadow.camera.far = 15;
+        this.sunLight.shadow.mapSize.set(1024, 1024);
+        this.sunLight.shadow.normalBias = 0.05;
+        this.sunLight.position.set(3.5, 2, -1.25);
+        this.scene.add(this.sunLight);
+    }
+    setEnvironmentMap() {
+        this.environmentMap = {}
+        this.environmentMap.intensity = 4.4
+        this.environmentMap.texture = this.resources.items.environmentMapTexture
+        this.environmentMap.texture.encoding = THREE.sRGBEncoding
+        this.scene.environment = this.environmentMap.texture
+
+        this.updateMaterials = () => {
+            this.scene.traverse((child) => {
+                if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+                    child.material.envMap = this.environmentMap.texture
+                    child.material.envMapIntensity = this.environmentMap.intensity
+                    child.material.needsUpdate = true
+                }
+            })
+        }
+
+        this.updateMaterials()
+    }
+}
