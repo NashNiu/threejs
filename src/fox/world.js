ac8f291420cb7322e9a1fd5b39290ad8773d6ad7
nash <545100402@qq.com>
2026-02-10 22:28:58 +0800

    feat: add environment class and setup in world

--- src/fox/world.js
+++ src/fox/world.js
@@ -1,15 +1,17 @@
 import * as THREE from "three";
 import Fox from "./fox";
-
+import Environment from "./environment";
 export default class World {
   constructor() {
     this.fox = new Fox();
     this.scene = this.fox.scene;
     const testMesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true }),
+      new THREE.MeshBasicMaterial(),
     );
     this.scene.add(testMesh);
+    // setup environment
+    this.environment = new Environment()
   }
 }
 
