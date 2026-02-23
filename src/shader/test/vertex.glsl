7bba1c526618eecbb8e83461f1488838ecc1c96a
nash <545100402@qq.com>
2026-02-23 21:04:51 +0800

    feat: add random vertex displacement and color variation

--- src/shader/test/vertex.glsl
+++ src/shader/test/vertex.glsl
@@ -2,11 +2,14 @@ uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 attribute vec3 position;
+attribute float aRandom;
+varying float vRandom;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 15.0) * 0.1;
+    modelPosition.z += aRandom * 0.1;
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
     gl_Position = projectionPosition;
+    vRandom = aRandom;
 }
