a0e4bf4145ea0bb95b259673a36f027290f093c4
nash <545100402@qq.com>
2026-03-12 00:02:15 +0800

    feat: add fireworks visualization with Three.js

---
+++ src/fireworks/shaders/vertex.glsl
@@ -0,0 +1,12 @@
+uniform float uSize;
+uniform vec2 uResolution;
+
+void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0); 
+    vec4 viewPosition = viewMatrix * modelPosition;
+    gl_Position = projectionMatrix * viewPosition;
+
+    //final size
+    gl_PointSize = uSize * uResolution.y;
+    gl_PointSize *= 1.0 / -viewPosition.z;
+}
