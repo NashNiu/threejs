03ca42b8b71bfd49dfa1d9a27159fbba27b30001
niutengfei <niutengfei@magiccompass.cn>
2026-03-16 18:03:45 +0800

    feat(shaders): add ambient light support to fragment shader

--- src/lightsShading/shaders/shading/fragment.glsl
+++ src/lightsShading/shaders/shading/fragment.glsl
@@ -1,8 +1,18 @@
 uniform vec3 uColor;
 
-void main()
-{
+#include ../includes/ambientLight.glsl
+
+void main(){
     vec3 color = uColor;
+
+    // light
+    vec3 light = vec3(0.0, 0.0, 0.0); // Light direction
+     // Add ambient light
+    light += ambientLight(
+        vec3(1.0, 1.0, 1.0), // lightColor
+        0.2 // lightIntensity
+    );
+    color *= light; // Apply light to the color
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
