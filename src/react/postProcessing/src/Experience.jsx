ae8f9a6caaf3ecc8133242ec8a0c80522943958c
niutengfei <niutengfei@magiccompass.cn>
2026-04-29 18:57:06 +0800

    feat: add postprocessing library and integrate EffectComposer in Experience component

--- src/react/postProcessing/src/Experience.jsx
+++ src/react/postProcessing/src/Experience.jsx
@@ -1,12 +1,46 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { EffectComposer } from "@react-three/postprocessing";
+import {
+  EffectComposer,
+  Vignette,
+  Glitch,
+  Noise,
+  Bloom,
+  DepthOfField,
+} from "@react-three/postprocessing";
+import { BlendFunction, GlitchMode } from "postprocessing";
 
 export default function Experience() {
   return (
     <>
+      <color attach="background" args={["#ffffff"]} />
       <EffectComposer>
-        
+        {/* <Vignette
+          eskil={false}
+          offset={0.5}
+          darkness={1.1}
+          blendFunction={BlendFunction.NORMAL}
+        /> */}
+        {/* <Glitch
+          delay={[0.5, 1.5]} // min and max glitch delay
+          duration={[0.6, 1.0]} // min and max glitch duration
+          strength={[0.3, 1.0]} // min and max glitch strength
+          mode={GlitchMode.SPORADIC} // glitch mode
+        /> */}
+        {/* <Noise
+          opacity={0.5} // noise opacity
+          BlendFunction={BlendFunction.AVERAGE}
+        /> */}
+        {/* <Bloom
+          mipmapBlur
+          luminanceThreshold={0}
+          intensity={0.5} // bloom intensity
+        /> */}
+        {/* <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={6}
+        /> */}
       </EffectComposer>
       <Perf position="top-left" />
 
