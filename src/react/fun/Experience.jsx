8434a9d07500b8ad8eefce8e70a4da0e20e2a33d
niutengfei <niutengfei@magiccompass.cn>
2026-04-30 17:42:55 +0800

    feat: add 3D MacBook Pro model to experience scene

--- src/react/fun/Experience.jsx
+++ src/react/fun/Experience.jsx
@@ -1,14 +1,26 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  PresentationControls,
+  useGLTF,
+  Environment,
+  Float,
+} from "@react-three/drei";
 
 export default function Experience() {
+  const computer = useGLTF("/macbook_pro/scene.gltf");
   return (
     <>
-      <OrbitControls makeDefault />
-
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Environment preset="city" />
+      <color args={["#241a1a"]} attach="background" />
+      <PresentationControls makeDefault>
+        <Float rotationIntensity={0.4}>
+          <primitive
+            object={computer.scene}
+            scale={0.07}
+            position={[0.3, -0.6, 0]}
+          />
+        </Float>
+      </PresentationControls>
     </>
   );
 }
+
